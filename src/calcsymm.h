1dc32c20825ff73470e6442f8a595190f9243b77
rokicki <rokicki@gmail.com>
2019-02-13 15:30:42 -0800

    Added a tiny bit more documentation.

--- src/calcsymm.h
+++ src/calcsymm.h
@@ -1,5 +1,12 @@
 #ifndef CALCSYMM_H
 #include "puzdef.h"
+/*
+ *   The major improvement needed for this program is to use symmetry to
+ *   reduce searches and improve pruning tables.  This code is an early
+ *   attempt to automatically determine the symmetry of a given puzzle.
+ *   It is not fully ready yet, and should be considered highly
+ *   experimental.
+ */
 void gensymm(const puzdef &pd) ;
 #define CALCSYMM_H
 #endif
--- src/canon.h
+++ src/canon.h
@@ -2,6 +2,16 @@
 #include <vector>
 #include "puzdef.h"
 using namespace std ;
+/*
+ *   This set of routines calculates the canonical sequences for a given
+ *   puzzle.  The normal, default version does this by calculating what
+ *   pairs of moves commute, and deriving a state graph from this.  The
+ *   second version uses a hashtable of identical states (if the group
+ *   is a graph) which lets it do more aggressive pruning at the cost of
+ *   additional memory usage.  With the show canonical states option
+ *   you can see how effective each strategy is for a given puzzle, and
+ *   from that decide which to use.
+ */
 void makecanonstates(puzdef &pd) ;
 extern vector<ull> canonmask ;
 extern vector<vector<int> > canonnext ;

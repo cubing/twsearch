83bba76f448d0ddb5eae5647a76876bc497aa365
Lucas Garron <code@garron.net>
2022-10-21 00:20:13 -0700

    Avoid an extra string interpolation for the base63 data URL.

--- script/generate-wasm-serialized.js
+++ script/generate-wasm-serialized.js
@@ -3,6 +3,5 @@ import { readFile, writeFile } from "fs/promises";
 const wasmInPath = new URL("../build/wasm-wrapped/twsearch.wasm", import.meta.url).pathname
 const wasmOutPath = new URL("../src/js/generated-wasm/twsearch.wasm.serialized.js", import.meta.url).pathname
 const wasmBuffer = await readFile(wasmInPath);
-await writeFile(wasmOutPath, `const wasmSourceBase64 = ${JSON.stringify(wasmBuffer.toString("base64"))};
-export const wasmSourceDataURL = \`data:application/wasm;base64,\${wasmSourceBase64}\`
+await writeFile(wasmOutPath, `export const wasmSourceDataURL = "data:application/wasm;base64,${wasmBuffer.toString("base64")}";
 `, "utf-8");

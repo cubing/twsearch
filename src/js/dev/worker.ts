7fb2382a41c29405f98605b0c056217275f2537c
Lucas Garron <code@garron.net>
2022-11-29 21:45:01 -0800

    Fix up tests.
    *shakes fist at https://github.com/nodejs/node/issues/43583 *

--- src/js/dev/worker.ts
+++ src/js/dev/worker.ts
@@ -2,38 +2,5 @@
 export const workerURL = import.meta.url;
 
 if (globalThis.WorkerGlobalScope) {
-  (async () => {
-    const [
-      { cube3x3x3 },
-      { setKPuzzleDefString, solveState, serializeDefToTws },
-    ] = await Promise.all([import("cubing/puzzles"), import("..")]);
-
-    await setKPuzzleDefString(
-      serializeDefToTws(await cube3x3x3.kpuzzle(), {
-        moveSubset: ["U", "L", "F", "R", "B", "D"],
-        startState: `EDGES
-  0 0 0 0 1 2 3 4 5 6 7 8
-  0 0 0 0 0 0 0 0 0 0 0 0
-  CORNERS
-  0 0 0 0 1 2 3 4
-  0 0 0 0 0 0 0 0
-  CENTERS
-  0 1 2 3 4 5
-  0 0 0 0 0 0`,
-      }),
-    );
-    (
-      await solveState(`ScrambleState test
-  EDGES
-  0 0 0 0 1 2 3 4 5 6 7 8
-  1 1 1 1 0 0 0 0 0 0 0 0
-  CORNERS
-  0 0 0 0 1 2 3 4
-  0 0 0 0 0 0 0 0
-  CENTERS
-  0 1 2 3 4 5
-  0 0 0 0 0 0
-  End`)
-    ).log();
-  })();
+  import("./test");
 }

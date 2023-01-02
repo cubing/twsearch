e6c4b1c68c33acd7257f8be48dc9af602243153e
rokicki <rokicki@gmail.com>
2023-01-02 10:33:29 -0800

    Fixed the Windows preprocessor symbol.

--- src/cpp/util.h
+++ src/cpp/util.h
@@ -2,7 +2,7 @@
 #include <string>
 #include <cstring>
 #include <cstdlib>   // for drand48 and friends
-#ifndef __WIN64__
+#ifndef _WIN64
 #include <strings.h> // for ffsll
 #endif
 using namespace std ;

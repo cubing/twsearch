105f8432b8e3abc44c3eb1b8af592874c0ce9377
rokicki <rokicki@gmail.com>
2023-01-04 16:29:48 -0800

    Allowed the restricted move list to include rotations.

--- src/cpp/filtermoves.cpp
+++ src/cpp/filtermoves.cpp
@@ -16,41 +16,49 @@ int goodmove(const moove &mv, int inc, int order) {
    return (mv.twist % inc == 0) ;
 }
 void filtermovelist(puzdef &pd, const char *movelist) {
-   vector<int> moves = parsemovelist(pd, movelist) ;
-   vector<int> lowinc(pd.basemoves.size()) ;
+   int nummoves = pd.moves.size() ;
+   int numbmoves = pd.basemoves.size() ;
+   vector<int> moves = parsemoveorrotationlist(pd, movelist) ;
+   vector<int> lowinc(pd.basemoves.size() + pd.rotations.size()) ;
    for (int i=0; i<(int)moves.size(); i++) {
-      moove &mv = pd.moves[moves[i]] ;
-      if (lowinc[mv.base])
+      moove &mv = moves[i] >= nummoves ?
+                pd.expandedrotations[moves[i]-nummoves] : pd.moves[moves[i]] ;
+      int obase = moves[i] >= nummoves ? numbmoves + mv.base : mv.base ;
+      if (lowinc[obase])
          error("Move list restriction should only list a base move once.") ;
-      lowinc[mv.base] = mv.twist ;
+      lowinc[obase] = mv.twist ;
    }
    vector<moove> newbase ;
    map<int, int> moveremap ;
    vector<int> newbasemoveorders ;
-   for (int i=0; i<(int)pd.basemoves.size(); i++)
-      if (goodmove(pd.basemoves[i], lowinc[i], pd.basemoveorders[i])) {
+   for (int i=0; i<(int)pd.basemoves.size() + (int)pd.baserotorders.size(); i++) {
+      moove &bm = i >= numbmoves ?  pd.rotations[i-numbmoves] : pd.basemoves[i] ;
+      int bmi = i >= numbmoves ? pd.baserotorders[i-numbmoves] : pd.basemoveorders[i] ;
+      if (goodmove(bm, lowinc[i], bmi)) {
          int newbasenum = newbase.size() ;
-         moove newmv = pd.basemoves[i] ;
+         moove newmv = bm ;
          newmv.base = newbasenum ;
          moveremap[i] = newbasenum ;
          newbase.push_back(newmv) ;
-         newbasemoveorders.push_back(pd.basemoveorders[i] / lowinc[i]) ;
-      } else {
+         newbasemoveorders.push_back(bmi / lowinc[i]) ;
       }
+   }
    vector<moove> newmvs ;
-   for (int i=0; i<(int)pd.moves.size(); i++) {
-      int obase = pd.moves[i].base ;
-      if (goodmove(pd.moves[i], lowinc[obase], pd.basemoveorders[obase])) {
-         moove newmv = pd.moves[i] ;
+   for (int i=0; i<(int)pd.moves.size() + (int)pd.rotations.size(); i++) {
+      moove &bm = i >= nummoves ? pd.expandedrotations[i-nummoves] : pd.moves[i] ;
+      int obase = i >= nummoves ? numbmoves + bm.base : bm.base ;
+      int bmi = obase >= numbmoves ? pd.baserotorders[obase-numbmoves] : pd.basemoveorders[obase] ;
+      if (goodmove(bm, lowinc[obase], bmi)) {
+         moove newmv = bm ;
          int otwist = newmv.twist ;
-         newmv.twist /= lowinc[pd.moves[i].base] ;
+         newmv.twist /= lowinc[obase] ;
          if (otwist == lowinc[obase] && lowinc[obase] > 1) {
             int newbasenum = newbase.size() ;
             moveremap[obase] = newbasenum ;
             newmv.base = newbasenum ;
             newmv.cost /= lowinc[obase] ;
             newbase.push_back(newmv) ;
-            newbasemoveorders.push_back(pd.basemoveorders[obase] / lowinc[obase]) ;
+            newbasemoveorders.push_back(bmi / lowinc[obase]) ;
          }
          newmv.base = moveremap[obase] ;
          newmvs.push_back(newmv) ;

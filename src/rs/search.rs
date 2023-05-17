330ad4795ff80e29255ef92059c71b311d37a846
Lucas Garron <code@garron.net>
2023-05-17 03:26:57 -0700

    [rust] Allow JSON file to contain a single scramble state.

--- src/rs/search.rs
+++ src/rs/search.rs
@@ -4,7 +4,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use cubing::kpuzzle::KPuzzleDefinition;
+use cubing::kpuzzle::{KPuzzleDefinition, KStateData};
 use serde::Deserialize;
 use tempfile::NamedTempFile;
 
@@ -12,8 +12,8 @@ use crate::{
     options::{reset_args_from, SetCppArgs},
     rust_api,
     serialize::{
-        serialize_kpuzzle_definition, serialize_scramble_list, KPuzzleSerializationOptions,
-        ScrambleList,
+        serialize_kpuzzle_definition, serialize_scramble_list, serialize_scramble_state_data,
+        KPuzzleSerializationOptions, ScrambleList,
     },
 };
 
@@ -21,22 +21,34 @@ use crate::{
 fn rewrite_input_file<T: for<'a> Deserialize<'a>>(
     input_file: &Path,
     rewrite_fn: fn(T) -> String,
-) -> (String, Option<NamedTempFile>) {
+) -> Result<(String, Option<NamedTempFile>), String> {
     format!("Rewriting: {:?}", input_file);
-    let input_str = read_to_string(input_file).expect("Could not read input file.");
-    let input_parsed: T = serde_json::from_str(&input_str).expect("Input file is not valid JSON.");
+    let input_str = read_to_string(input_file).or(Err("Could not read input file."))?;
+    let input_parsed: T =
+        serde_json::from_str(&input_str).or(Err("Input file is not valid JSON."))?;
     let output_str = rewrite_fn(input_parsed);
-    let mut temp_file = NamedTempFile::new().expect("Could not create a temp file.");
+    let mut temp_file = NamedTempFile::new().or(Err("Could not create a temp file."))?;
     temp_file
         .write_all(output_str.as_bytes())
-        .expect("Could not write a rewritten input file.");
+        .or(Err("Could not write a rewritten input file."))?;
 
-    let s = temp_file
-        .path()
-        .to_str()
-        .expect("Used an invalid temporary file path.")
-        .to_owned();
-    (s, Some(temp_file))
+    let s = match temp_file.path().to_str() {
+        Some(s) => s,
+        None => return Err("Used an invalid temporary file path.".to_owned()),
+    };
+    Ok((s.to_owned(), Some(temp_file)))
+}
+
+fn must_rewrite_input_file<T: for<'a> Deserialize<'a>>(
+    input_file: &Path,
+    rewrite_fn: fn(T) -> String,
+) -> (String, Option<NamedTempFile>) {
+    match rewrite_input_file(input_file, rewrite_fn) {
+        Ok(v) => v,
+        Err(e) => {
+            panic!("{}", e)
+        }
+    }
 }
 
 pub fn main_search(
@@ -52,7 +64,7 @@ pub fn main_search(
             None::<NamedTempFile>,
         ),
         _ => {
-            rewrite_input_file(def_file, |def: KPuzzleDefinition| {
+            must_rewrite_input_file(def_file, |def: KPuzzleDefinition| {
                 serialize_kpuzzle_definition(
                     def,
                     Some(&KPuzzleSerializationOptions {
@@ -75,9 +87,16 @@ pub fn main_search(
                     .to_owned(),
                 None::<NamedTempFile>,
             ),
-            _ => rewrite_input_file(scramble_file, |scramble_list: ScrambleList| {
-                serialize_scramble_list(&scramble_list)
-            }),
+            _ => {
+                match rewrite_input_file(scramble_file, |scramble_list: ScrambleList| {
+                    serialize_scramble_list(&scramble_list)
+                }) {
+                    Ok(v) => v,
+                    Err(_) => must_rewrite_input_file(scramble_file, |kstate_data: KStateData| {
+                        serialize_scramble_state_data("Anonymous_Scramble", &kstate_data)
+                    }),
+                }
+            }
         },
         None => ("".to_owned(), None),
     };

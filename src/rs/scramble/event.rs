d9f63af6ff8ce958b787dbbf854b683ec4593977
Lucas Garron <code@garron.net>
2023-10-15 20:17:00 -0700

    [rust] Add a way to convert `&str` to `Event`.

--- src/rs/scramble/event.rs
+++ src/rs/scramble/event.rs
@@ -1,7 +1,26 @@
+use crate::_internal::SearchError;
+
+// TODO: move this to another export location.
 pub enum Event {
     Cube2x2x2,
     Cube3x3x3,
     Pyraminx,
+}
+
+impl TryFrom<&str> for Event {
+    type Error = SearchError;
+
+    fn try_from(event_str: &str) -> Result<Self, Self::Error> {
+        Ok(match event_str {
+            "222" => Event::Cube2x2x2,
+            "333" => Event::Cube3x3x3,
+            _ => {
+                return Err(SearchError {
+                    description: format!("Unknown event ID: {}", event_str),
+                })
+            }
+        })
+    }
 }
 
 impl Event {

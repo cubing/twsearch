02a57f7f0f6a9b57506b018dbf24a1d6b860612b
Lucas Garron <code@garron.net>
2024-09-13 17:48:53 -0700

    Modify the Skewb definition to ignore CO for one piece per corner orbit.
    For each of the two orbits, this piece's orientation is constrained by the other orbit's permutation. Instead of calculating the appropriate value, we can just perform the search ignoring the orientation and be confident that this corresponds to exactly one valid state.

--- src/rs/scramble/puzzles/skewb.rs
+++ src/rs/scramble/puzzles/skewb.rs
@@ -1,4 +1,4 @@
-use cubing::alg::{ Alg};
+use cubing::alg::Alg;
 
 
 use super::{
@@ -6,11 +6,11 @@ use super::{
         randomize_orbit_naïve, OrbitOrientationConstraint, OrbitPermutationConstraint,
     },
     super::scramble_search::{filtered_search, generators_from_vec_str},
-    definitions::skewb_fixed_corner_kpuzzle,
+    definitions::skewb_fixed_corner_with_co_tweaks_kpuzzle,
 };
 
 pub fn scramble_skewb() -> Alg {
-    let kpuzzle = skewb_fixed_corner_kpuzzle();
+    let kpuzzle = skewb_fixed_corner_with_co_tweaks_kpuzzle();
     loop {
         let mut scramble_pattern = kpuzzle.default_pattern();
 
@@ -20,7 +20,7 @@ pub fn scramble_skewb() -> Alg {
             &mut scramble_pattern,
             orbit_info,
             OrbitPermutationConstraint::SingleOrbitEvenParity,
-            OrbitOrientationConstraint::AnySum,
+            OrbitOrientationConstraint::SetHighestNumberedPieceToIgnoredOrientation,
         );
         
         let orbit_info = &kpuzzle.data.ordered_orbit_info[1];
@@ -29,9 +29,9 @@ pub fn scramble_skewb() -> Alg {
             &mut scramble_pattern,
             orbit_info,
             OrbitPermutationConstraint::SingleOrbitEvenParity,
-            OrbitOrientationConstraint::AnySum,
+            OrbitOrientationConstraint::SetHighestNumberedPieceToIgnoredOrientation,
         );
-        
+
         let orbit_info = &kpuzzle.data.ordered_orbit_info[2];
         assert_eq!(orbit_info.name.0, "CENTERS");
         randomize_orbit_naïve(
@@ -40,14 +40,6 @@ pub fn scramble_skewb() -> Alg {
             OrbitPermutationConstraint::SingleOrbitEvenParity,
             OrbitOrientationConstraint::OrientationsMustSumToZero,
         );
-
-        // TODO: for a valid scramble, the orientation of each corner orbit depends on the permutation
-        // of the other orbit. We don't have a simple way to do generate right now,
-        // so we do extra rejection sampling to find a valid scramble.
-        // In the future, we can do one of:
-        // - Implement the math for this.
-        // - Generate states using Schreier-Sims/SGS.
-        // - Do an additional rejection sampling pass ignoring centers?
 
         let generators = generators_from_vec_str(vec!["U", "L", "R", "B"]); // TODO: cache
         if let Some(scramble) = filtered_search(&scramble_pattern, generators, 7, Some(11), Some(12)) {

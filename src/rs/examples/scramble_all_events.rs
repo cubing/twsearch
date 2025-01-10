76f3df8676cf91d668c128042bd9f003f78eb6e5
Lucas Garron <code@garron.net>
2025-01-10 07:08:21 -0800

    Rename `random_scramble_for_event` example to `scramble_all_events` and include each event exactly once.

--- src/rs/examples/scramble_all_events.rs
+++ src/rs/examples/scramble_all_events.rs
@@ -1,41 +1,32 @@
+// Run using: cargo run --release --example scramble_all_events
+
 use instant::Instant;
 use twsearch::scramble::{random_scramble_for_event, Event};
 
 pub fn main() {
     for event in [
         Event::Cube3x3x3Speedsolving,
-        Event::Cube3x3x3Speedsolving,
-        Event::Cube3x3x3Speedsolving,
-        Event::Cube3x3x3Speedsolving,
-        Event::Cube3x3x3Speedsolving,
         Event::Cube2x2x2Speedsolving,
+        //Event::Cube4x4x4Speedsolving
         Event::Cube5x5x5Speedsolving,
         Event::Cube6x6x6Speedsolving,
         Event::Cube7x7x7Speedsolving,
         Event::Cube3x3x3Blindfolded,
         Event::Cube3x3x3FewestMoves,
         Event::Cube3x3x3OneHanded,
+        Event::ClockSpeedsolving,
         Event::MegaminxSpeedsolving,
         Event::PyraminxSpeedsolving,
+        Event::SkewbSpeedsolving,
+        Event::Square1Speedsolving,
+        //Event::Cube4x4x4Blindfolded
         Event::Cube5x5x5Blindfolded,
         Event::Cube3x3x3MultiBlind,
-        Event::ClockSpeedsolving,
-        Event::SkewbSpeedsolving,
-        // Event::Square1Speedsolving, // TODO: re-enable after we are performant enough.
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
-        Event::Cube3x3x3FewestMoves,
+        //Event::FTOSpeedsolving
+        //Event::MasterTetraminxSpeedsolving
+        //Event::KilominxSpeedsolving
+        //Event::RediCubeSpeedsolving
+        Event::BabyFTOSpeedsolving,
     ] {
         let start_time = Instant::now();
         let scramble = random_scramble_for_event(event).unwrap();

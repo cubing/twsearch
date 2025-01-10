a307bf0bcbb18df2f763c6a3a86c2184b70fe3e3
Lucas Garron <code@garron.net>
2025-01-10 05:21:14 -0800

    [triple_phase_coordinate_puzzle.rs] Short-circuit failing transformation application again.

--- src/rs/_internal/search/coordinates/triple_phase_coordinate_puzzle.rs
+++ src/rs/_internal/search/coordinates/triple_phase_coordinate_puzzle.rs
@@ -231,20 +231,20 @@ impl<
         let coordinate1 = self
             .data
             .puzzle1
-            .pattern_apply_transformation(&pattern.coordinate1, transformation_to_apply);
+            .pattern_apply_transformation(&pattern.coordinate1, transformation_to_apply)?;
         let coordinate2 = self
             .data
             .puzzle2
-            .pattern_apply_transformation(&pattern.coordinate2, transformation_to_apply);
+            .pattern_apply_transformation(&pattern.coordinate2, transformation_to_apply)?;
         let coordinate3 = self
             .data
             .puzzle3
-            .pattern_apply_transformation(&pattern.coordinate3, transformation_to_apply);
+            .pattern_apply_transformation(&pattern.coordinate3, transformation_to_apply)?;
 
         Some(Self::Pattern {
-            coordinate1: coordinate1?,
-            coordinate2: coordinate2?,
-            coordinate3: coordinate3?,
+            coordinate1,
+            coordinate2,
+            coordinate3,
         })
     }
 

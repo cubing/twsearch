8630feab2c1598297e0ded0f0646820482237321
Lucas Garron <code@garron.net>
2025-01-12 04:18:07 -0800

    [search_adaptations.rs] Update doc wording and cross-linking.

--- src/rs/_internal/search/idf_search/search_adaptations.rs
+++ src/rs/_internal/search/idf_search/search_adaptations.rs
@@ -7,17 +7,18 @@ use crate::_internal::puzzle_traits::puzzle_traits::{
 };
 
 use super::super::{
-    pattern_validity_checker::{AlwaysValid, PatternValidityChecker},
     hash_prune_table::HashPruneTable,
+    pattern_validity_checker::{AlwaysValid, PatternValidityChecker},
     prune_table_trait::PruneTable,
 };
 
-/// The `SearchAdaptations` trait bundles various traits that IDFS can invoke to
-/// change its search behaviour.
+/// The [`SearchAdaptations`] trait bundles various traits that IDFS can invoke
+/// to change its search behaviour.
 ///
-/// Each associated type could theoretically be a type parameter on `IDFSearch`
-/// itself, but this would make `IDFSearch` types rather unwieldy. So instead of
-/// this:
+/// Each associated type could theoretically be a type parameter on
+/// [`IDFSearch`](super::idf_search::IDFSearch) itself, but this would make
+/// [`IDFSearch`](super::idf_search::IDFSearch) types rather unwieldy. So
+/// instead of this:
 ///
 ///    IDFSearch<TPuzzle: …, PatternValidityChecker: …, PruneTable: …, (more in
 ///    the future…)>
@@ -26,17 +27,19 @@ use super::super::{
 ///
 ///    IDFSearch<TPuzzle: …, Adaptations: …>
 ///
-/// In addition, the `DefaultSearchAdaptations` trait can be implemented for any
-/// given `SemiGroupActionPuzzle`. This allows us to avoid specifying the
-/// `Adaptations` associated type in common. For example, the type for a
-/// `KPuzzle` search will often just be:
+/// In addition, the [`DefaultSearchAdaptations`] trait can be implemented for
+/// any given [`SemiGroupActionPuzzle`]. This allows us to avoid specifying the
+/// `Adaptations` type parameter on [`IDFSearch`](super::idf_search::IDFSearch)
+/// in common situations. For example, the type for a [`KPuzzle`] search will
+/// often just be:
 ///
 ///     IDFSearch<KPuzzle>
 ///
 /// Note: the main reason that these are traits is that it enables "zero-cost"
 /// abstraction in code that is run tens of millions of times per second. If you
 /// implement a trait with trivial code, then this code can be inlined in an
-/// instantiated `IDFSearch` without dynamic dispatch.
+/// instantiated [`IDFSearch`](super::idf_search::IDFSearch) without dynamic
+/// dispatch.
 ///
 /// TODO: figure out if/when dynamic dispatch is actually cheap and ergonomic
 /// enough once we know all the adaptations we need for common puzzles.

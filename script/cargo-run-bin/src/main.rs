db8e4a2932ebe4aa4773e1ab6b10678726419fb0
Lucas Garron <code@garron.net>
2025-01-19 03:23:45 -0800

    Simplify error.

--- script/cargo-run-bin/src/main.rs
+++ script/cargo-run-bin/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, process};
+use std::process;
 
 fn main() {
     let res = cargo_run_bin::cli::run();
@@ -6,7 +6,7 @@ fn main() {
     // Only reached if run-bin code fails, otherwise process exits early from within
     // binary::run.
     if let Err(res) = res {
-        eprintln!("\x1b[31m{}\x1b[0m", format!("run-bin failed: {res}"));
+        eprintln!("run-bin failed: {res}");
         process::exit(1);
     }
 }

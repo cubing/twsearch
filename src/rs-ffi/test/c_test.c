6ef9a72fec6fac5cf4c26eeac4d39c1521aa110c
Lucas Garron <code@garron.net>
2025-03-10 23:44:37 -0700

    [c_test.c] Refactor out common code.

--- src/rs-ffi/test/c_test.c
+++ src/rs-ffi/test/c_test.c
@@ -1,39 +1,25 @@
 #include <stdio.h>
 #include "../../../target/release/libtwsearch_ffi.h"
 
+
+void generate_scramble(const char* event_id) {
+  printf("// %s\n", event_id);
+  puts(ffi_random_scramble_for_event(event_id));
+
+}
+
 int main() {
-  puts("// 222");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_222));
-
-  puts("// pyram");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_PYRAM));
-
-  puts("// minx");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_MINX));
-
-  puts("// 333");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_333));
-
-  puts("// 333bf");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_333BF));
-
-  puts("// 333fm");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_333FM));
-
-  puts("// 555");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_555));
-
-  puts("// 666");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_666));
-
-  puts("// 777");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_777));
-
-  puts("// skewb");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_SKEWB));
-
-  puts("// sq1");
-  puts(ffi_random_scramble_for_event(CUBING_EVENT_SQ1));
+  generate_scramble(CUBING_EVENT_222);
+  generate_scramble(CUBING_EVENT_PYRAM);
+  generate_scramble(CUBING_EVENT_MINX);
+  generate_scramble(CUBING_EVENT_333);
+  generate_scramble(CUBING_EVENT_333BF);
+  generate_scramble(CUBING_EVENT_333FM);
+  generate_scramble(CUBING_EVENT_555);
+  generate_scramble(CUBING_EVENT_666);
+  generate_scramble(CUBING_EVENT_777);
+  generate_scramble(CUBING_EVENT_SKEWB);
+  generate_scramble(CUBING_EVENT_SQ1);
 
   printf("Freed %u scramble finder(s).\n", ffi_free_memory_for_all_scramble_finders()); 
 }

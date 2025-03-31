0109bf21b0083dfe82b2399d87ec5933886850e7
Lucas Garron <code@garron.net>
2025-03-31 05:28:14 -0700

    [cli_scramble.rs] Print duration info to stderr.

--- src/rs/_cli/commands/cli_scramble.rs
+++ src/rs/_cli/commands/cli_scramble.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use cubing::alg::{
     experimental_twizzle_link::{experimental_twizzle_link, ExperimentalTwizzleLinkParameters},
     Alg,
@@ -11,13 +13,27 @@ use twsearch::{
 };
 
 pub fn cli_scramble(args: &ScrambleArgs) -> Result<(), CommandError> {
-    let l = args.event_id.as_str();
-    let a = Event::try_from(l);
-    let event = a?;
+    let event_id = args.event_id.as_str();
+    let event = Event::try_from(event_id)?;
 
-    for _ in 0..args.amount {
+    let total_start_time = Instant::now();
+    for i in 1..=args.amount {
+        let current_scramble_start_time = Instant::now();
         let scramble = random_scramble_for_event(event)?;
+        eprintln!(
+            "// Scramble #{} found in: {:?}",
+            i,
+            Instant::now() - current_scramble_start_time
+        );
         println!("{}", scramble);
+        let elapsed_duration = Instant::now() - total_start_time;
+        eprintln!(
+            "Found {} scramble{} in {:?} so far (average: {:?} per scramble)",
+            i,
+            if i == 1 { "" } else { "s" },
+            elapsed_duration,
+            elapsed_duration / (i as u32)
+        );
     }
 
     Ok(())
@@ -26,16 +42,20 @@ pub fn cli_scramble(args: &ScrambleArgs) -> Result<(), CommandError> {
 pub fn cli_scramble_finder_solve(args: &ScrambleFinderArgs) -> Result<(), CommandError> {
     match &args.command {
         ScrambleFinderCommand::Solve(scramble_finder_solve_args) => {
-            let l = scramble_finder_solve_args.event_id.as_str();
-            let a = Event::try_from(l);
-            let event = a?;
+            let event_id = scramble_finder_solve_args.event_id.as_str();
+            let event = Event::try_from(event_id)?;
 
             let scramble_setup_alg = scramble_finder_solve_args
                 .scramble_setup_alg
                 .parse::<Alg>()
                 .expect("Invalid alg");
 
+            let current_scramble_start_time = Instant::now();
             let scramble = scramble_finder_solve(event, &scramble_setup_alg)?;
+            eprintln!(
+                "// Scramble found in: {:?}",
+                Instant::now() - current_scramble_start_time
+            );
             println!("{}", scramble);
             if scramble_finder_solve_args.print_link {
                 let link = experimental_twizzle_link(ExperimentalTwizzleLinkParameters {

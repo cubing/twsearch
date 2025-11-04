44a533050ede67cb560bac44b2b51c0942c39669
Gregor Billing <gregor.billing.dev@gmail.com>
2025-11-04 10:40:06 +0100

    Add binding for deriveScrambleForEvent

--- src/ruby-gem/ext/twsearch/src/lib.rs
+++ src/ruby-gem/ext/twsearch/src/lib.rs
@@ -1,6 +1,8 @@
+use std::str::FromStr;
+
 use magnus::{function, prelude::*, Error, Ruby};
 
-use twsearch::scramble::{random_scramble_for_event, Event};
+use twsearch::scramble::{random_scramble_for_event, derive_scramble_for_event_seeded, Event, DerivationSeed, DerivationSalt};
 
 fn rb_random_scramble_for_event(ruby: &Ruby, event_str: String) -> Result<String, Error> {
     let event = Event::try_from(event_str.as_str())
@@ -11,6 +13,30 @@ fn rb_random_scramble_for_event(ruby: &Ruby, event_str: String) -> Result<String
         .map_err(|e| Error::new(ruby.exception_runtime_error(), e.description))
 }
 
+fn rb_derive_scramble_for_event(ruby: &Ruby, event_str: String, hex_derivation_seed_str: String, derivation_salt_hierarchy: Vec<String>) -> Result<String, Error> {
+    let event = Event::try_from(event_str.as_str())
+        .map_err(|e| Error::new(ruby.exception_arg_error(), e.description))?;
+
+    let root_derivation_seed = DerivationSeed::from_str(&hex_derivation_seed_str)
+        .map_err(|e| Error::new(ruby.exception_arg_error(), e))?;
+
+    let derivation_seed = if derivation_salt_hierarchy.is_empty() {
+        root_derivation_seed
+    } else {
+        let hierarchy: Vec<DerivationSalt> = derivation_salt_hierarchy.iter()
+            .map(|s| s.as_str())
+            .map(DerivationSalt::from_str)
+            .collect::<Result<Vec<DerivationSalt>, String>>()
+            .map_err(|e| Error::new(ruby.exception_arg_error(), e))?;
+
+        root_derivation_seed.derive_hierarchy(&hierarchy)
+    };
+
+    derive_scramble_for_event_seeded(&derivation_seed, event)
+        .map(|scramble| scramble.to_string())
+        .map_err(|e| Error::new(ruby.exception_runtime_error(), e))
+}
+
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("Twsearch")?;
@@ -18,6 +44,11 @@ fn init(ruby: &Ruby) -> Result<(), Error> {
     module.define_singleton_method(
         "random_scramble_for_event",
         function!(rb_random_scramble_for_event, 1),
+    )?;
+
+    module.define_singleton_method(
+        "derive_scramble_for_event",
+        function!(rb_derive_scramble_for_event, 3),
     )?;
 
     Ok(())

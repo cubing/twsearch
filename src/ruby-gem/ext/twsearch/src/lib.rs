6a6e87f1624e55a3dd351feebcd78ad41e25f8a0
Gregor Billing <gregor.billing.dev@gmail.com>
2025-11-04 09:27:09 +0100

    Update Magnus bindings to v0.8

--- src/ruby-gem/ext/twsearch/src/lib.rs
+++ src/ruby-gem/ext/twsearch/src/lib.rs
@@ -1,20 +1,19 @@
-use magnus::{define_module, function, prelude::*, Error};
+use magnus::{function, prelude::*, Error, Ruby};
 
 use twsearch::scramble::{random_scramble_for_event, Event};
 
-#[no_mangle]
-fn rb_random_scramble_for_event(event_str: String) -> Result<String, Error> {
+fn rb_random_scramble_for_event(ruby: &Ruby, event_str: String) -> Result<String, Error> {
     let event = Event::try_from(event_str.as_str())
-        .map_err(|e| Error::new(magnus::exception::arg_error(), e.description))?;
+        .map_err(|e| Error::new(ruby.exception_arg_error(), e.description))?;
 
     random_scramble_for_event(event)
         .map(|scramble| scramble.to_string())
-        .map_err(|e| Error::new(magnus::exception::runtime_error(), e.description))
+        .map_err(|e| Error::new(ruby.exception_runtime_error(), e.description))
 }
 
 #[magnus::init]
-fn init() -> Result<(), Error> {
-    let module = define_module("Twsearch")?;
+fn init(ruby: &Ruby) -> Result<(), Error> {
+    let module = ruby.define_module("Twsearch")?;
 
     module.define_singleton_method(
         "random_scramble_for_event",
